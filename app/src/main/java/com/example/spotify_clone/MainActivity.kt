bfa25de581de8a6f00edf57040f25fa9ff525230
Stefan <vucanstefan@gmail.com>
2023-08-24 14:17:00 +0200

    Initial commit

---
+++ app/src/main/java/com/example/spotify_clone/MainActivity.kt
@@ -0,0 +1,49 @@
+package com.example.spotify_clone
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.spotify_clone.ui.theme.Spotify_cloneTheme
+
+/**
+ * 24.08.2023 Spotify Clone app
+ */
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Spotify_cloneTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Spotify_cloneTheme {
+        Greeting("Android")
+    }
+}
